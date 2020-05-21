5624224bb6292cbc6c19aad8421f7f43623eca25
amitjamwal85@gmail.com <amitjamwal85@gmail.com>
2020-05-21 11:05:54 +0530

    update

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -3,14 +3,14 @@ import "firebase/messaging";
 import "firebase/firestore";
 
 const config = {
-  apiKey: "AIzaSyCr794C5fS5JsSSvNmbKgd-TuQR3aGFIW0",
-  authDomain: "vue-chat-56ca7.firebaseapp.com",
-  databaseURL: "https://vue-chat-56ca7.firebaseio.com",
-  projectId: "vue-chat-56ca7",
-  storageBucket: "vue-chat-56ca7.appspot.com",
-  messagingSenderId: "802914483090",
-  appId: "1:802914483090:web:b1530852302d0c8f255f4b",
-  measurementId: "G-JXW02Y4X6G"
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_DATABASE_URL,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_APP_ID,
+  measurementId: process.env.VUE_APP_MEASUREMENT_ID
 };
 
 firebase.initializeApp(config);
